3e150247c68aed5d3bc57b1c117534c38d740342
Emilien Socchi <emilien.socchi@gmail.com>
2025-01-24 14:37:30 +0100

    Add secret- and key-cred deletion to output

--- Tools/scripts/Find Multi-tenant Apps with unlocked App Instance Properties/Find-UnlockedMultiTenantApps.ps1
+++ Tools/scripts/Find Multi-tenant Apps with unlocked App Instance Properties/Find-UnlockedMultiTenantApps.ps1
@@ -3,17 +3,23 @@
 # Verifies whether multi-tenant non-Microsoft service principals are locking the creation of new password and key credentials (certificates) 
 # to prevent their compromise (refered to as "Credentials used for verification" in App Instance Property lock).
 #
-# More info: 
-#    https://learn.microsoft.com/en-us/entra/identity-platform/howto-configure-app-instance-property-locks
-# 
+# The goal is to detect applications hosted by vendors, which require sensitive permissions in our tenant, but do not protect their service 
+# principals from a compromise.
+#
+#
 # Requirements:
 #    An access token with the following MS Graph permissions (as application or delegated permissions):
 #       - Application.ReadWrite.All 
 #       - Directory.Read.All
 #
-# Warning:
+#
+# WARNING:
 #   For some unknow reason, removing the certificate uploaded to test the 'keyCredentials' property sometimes fail.
-#   Note that uploaded certificates are set to expire after 1 minute.
+#   Note that the uploaded certificate is set to expire after 1 minute.
+#
+#
+# More info: 
+#    https://learn.microsoft.com/en-us/entra/identity-platform/howto-configure-app-instance-property-locks
 #
 ####
 
@@ -80,12 +86,12 @@ $pfxLocation = "$PSScriptRoot\dummy.pfx"            # pfx file for the dummy cer
 
 # Authentication
 # Interactive delegated authentication
-#Connect-MgGraph -Scopes "Application.ReadWrite.All", "Directory.Read.All"
+#Connect-MgGraph -Scopes "Application.ReadWrite.All", "Directory.Read.All" -ErrorAction Stop
 
 # Pass-the-token authentication
 $msgraphToken  = ''
 $secureToken = ConvertTo-SecureString -String $msgraphToken -AsPlainText -Force 
-Connect-MgGraph -NoWelcome -AccessToken $secureToken 
+Connect-MgGraph -NoWelcome -AccessToken $secureToken -ErrorAction Stop
 
 # Testing SPs
 $tenantId = (Get-MgOrganization).Id
@@ -107,6 +113,8 @@ foreach ($servicePrincipal in $allServicePrincipals) {
             DisplayName = $servicePrincipalName
             IsPasswordCredsLocked = $false
             IsKeyCredsLocked = $false
+            IsDummyPasswordRemoved = $false
+            IsDummyKeyRemoved = $false
         }
 
         ## Testing the 'passwordCredentials' property
@@ -121,6 +129,7 @@ foreach ($servicePrincipal in $allServicePrincipals) {
                 # The 'passwordCredentials' property of the SP is not protected
                 $testedServicePrincipal.IsPasswordCredsLocked = $false
                 Remove-MgServicePrincipalPassword -ServicePrincipalId $servicePrincipalId -KeyId $credential.KeyId
+                $testedServicePrincipal.IsDummyPasswordRemoved = $true
             }
         } catch {
             $defaultInstancePropertyLockError = 'CannotUpdateLockedServicePrincipalProperty'
@@ -138,33 +147,40 @@ foreach ($servicePrincipal in $allServicePrincipals) {
 
         ## Testing the 'keyCredentials' property (certificate)
         try {
+            $certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certificateLocation)
             $keyCredential = New-Object -TypeName Microsoft.Graph.PowerShell.Models.MicrosoftGraphKeyCredential
             $keyCredential.DisplayName = 'Locked down keyCredentials?'
             $keyCredential.EndDateTime = (Get-Date).AddMinutes(1)
             $keyCredential.StartDateTime = Get-Date
             $keyCredential.Type = 'AsymmetricX509Cert'
             $keyCredential.Usage = 'Verify'
-            
-            $certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certificateLocation)
+            $keyCredential.KeyId = [guid]::NewGuid()
             $keyCredential.Key = [System.Convert]::FromBase64String([System.Convert]::ToBase64String($certificate.RawData))
-
             Update-MgServicePrincipal -ServicePrincipalId $servicePrincipalId -KeyCredential @($keyCredential)
 
-            if ($credential) {
+            $servicePrincipal = Get-MgServicePrincipal -ServicePrincipalId $servicePrincipalId
+            $createdKeyCredential = $servicePrincipal.KeyCredentials | Where-Object { $_.KeyId -eq $keyCredential.KeyId }
+        
+            if ($createdKeyCredential) {
                 # The 'keyCredentials' property of the SP is not protected
                 $testedServicePrincipal.IsKeyCredsLocked = $false
                 $proofOfPossession = Get-ProofOfPossession -servicePrincipalId $servicePrincipalId -pfxLocation $pfxLocation
-                Remove-MgServicePrincipalKey -ServicePrincipalId $servicePrincipalId -KeyId $credential.KeyId -Proof $proofOfPossession
+                Remove-MgServicePrincipalKey -ServicePrincipalId $servicePrincipalId -KeyId $credential.KeyId -Proof $proofOfPossession -ErrorAction Stop # this breaks sometimes
+                $testedServicePrincipal.IsDummyKeyRemoved = $true
             }
         } catch {
             $defaultInstancePropertyLockError = 'Request_BadRequest'
+            $malformedProofError = 'Authentication_MissingOrMalformed'
             $errorRecord = $_
             $errorCode = $errorRecord.Exception.ErrorCode
-
+        
             if ($errorCode -eq $defaultInstancePropertyLockError) {
                 # The 'keyCredentials' property of the SP is protected as expected
                 $testedServicePrincipal.IsKeyCredsLocked = $true   
                 
+            } elseif ($errorCode -eq $null -and $errorRecord.FullyQualifiedErrorId -like "$malformedProofError*") {
+                # This is what happens when it breaks
+                $testedServicePrincipal.IsDummyKeyRemoved = $false
             } else {
                 Write-Host "An error occurred: $errorRecord"
             }
